77f0a6c293fc48b635f02adbde1fa258af9e103f
Omar Alvarez <omajul2@hotmail.com>
2016-05-16 16:12:54 +0200

    Create space model

---
+++ app/models/space.rb
@@ -0,0 +1,18 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+class Space
+  
+  include DataMapper::Resource
+
+  has n, :requests
+  belongs_to :user
+
+  property :id, Serial
+  property :name, String, required: true
+  property :description, String, required: true
+  property :price, Float, required: true
+  property :available_from, Date, required: true
+  property :available_to, Date, required: true
+
+end
