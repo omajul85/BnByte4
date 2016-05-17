fb83c37a38d9d4b64084f6fb2e816a238eb2bf55
Omar Alvarez <omajul2@hotmail.com>
2016-05-17 16:17:58 +0200

    Update session helpers and front-end date validation

--- spec/features/adding_spaces_spec.rb
+++ spec/features/adding_spaces_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 feature 'Adding spaces'  do
-	let!(:user) do
-    User.create(name: 'Foo Bar', username: 'foobar', email: 'foo@bar.com',
-      password: 'foobar', password_confirmation: 'foobar')
+	before(:each) do
+    User.create(name: 'Foo Bar',
+      username: 'fooby',
+      email: 'foo@bar.com',
+      password: 'foobar',
+      password_confirmation: 'foobar')
   end
 
 	scenario 'host can add a new space when logged in' do
-    sign_up
-    #expect(current_path).to eq('/')
-    expect{ create_space }.to change(Space, :count ).by(1) 
+    sign_in(email: 'foo@bar.com', password: 'foobar')
+    create_space(description: 'This is a test space')
     expect(page).to have_content('This is a test space')
 	end
 end
