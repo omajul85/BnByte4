4007460211fe5c0b4c94feccf0ae54eccf72d9a5
Jack Riminton <jriminton@gmail.com>
2016-05-18 11:33:02 +0100

    space description character length bug

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class Space
-  
+
   include DataMapper::Resource
 
   # has n, :requests
@@ -10,7 +10,8 @@ class Space
 
   property :id, Serial
   property :name, String, required: true
-  property :description, String, required: true
+  property :description, Text, required: true
+  validates_length_of :description, max: 1000
   property :price, String, required: true
   property :available_from, Date, required: true
   property :available_to, Date, required: true
