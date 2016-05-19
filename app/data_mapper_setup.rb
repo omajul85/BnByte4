32ae1852875bb3e98799b750d912eb10c7fa1b72
Jack Riminton <jriminton@gmail.com>
2016-05-19 11:47:14 +0100

    bug fixes from master

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,7 +5,7 @@ require_relative './models/space'
 require_relative './models/user'
 require_relative './models/request'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:admin@localhost/makersbnb_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 
 if ENV['RACK_ENV'] == 'test'
